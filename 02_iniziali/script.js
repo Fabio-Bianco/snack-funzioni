1506893fd4eec13e72881f638f4acb32c1c45e10
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-03 19:50:58 +0100

    Ex 2 iniziali: solved

---
+++ 02_iniziali/script.js
@@ -0,0 +1,20 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+console.log(names);
+
+// Dichiara la funzione qui.
+
+function getInitials(arr) {
+    let initials = [];
+    for (let i = 0; i < arr.length; i++) {
+        initials.push(arr[i][0]);
+    }
+    return initials;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(getInitials(names));
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
