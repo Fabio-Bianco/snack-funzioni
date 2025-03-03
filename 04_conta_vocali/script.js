ac2e8e774e232b1d43b8ee72e3d81ae89140acef
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-03 23:39:02 +0100

    Ex 03: solved with includes

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -26,6 +26,20 @@ function countVowels(str) {
 
   console.log(countVowels(word)); 
   
-
-
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+// RISULTATO UTILIZZANDO INCLUDES
+
+// function countVowels(str) {
+//     const vowels = 'aeiou'; 
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.includes(str[i].toLowerCase())) {
+//           count++;
+//         }
+//       }
+    
+//       return count;
+//     }
+
+//     console.log(countVowels(word));
